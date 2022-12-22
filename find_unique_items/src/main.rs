d9426fbca5c3aadaffa5ccc302b7494f289959d4
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-12-22 14:33:26 +0300

    Update

--- find_unique_items/src/main.rs
+++ find_unique_items/src/main.rs
@@ -1,60 +1,78 @@
-/*
-    Print Any Text Type
-
-    Write a function that can accept a String or a &str
-*/
-
-fn info(a: &T) {
-    todo!();
+fn unique(mut a: Vec<i32>) -> Vec<i32> {
+    a.sort();
+    a.dedup();
+    a
 }
+
+// // advanced 1.2: use generic types
+// fn unique<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+//     a.sort();
+//     a.dedup();
+//     a
+// }
+
+// // advanced 1.1: use generic types
+// fn unique<T: PartialOrd>(mut a: Vec<T>) -> Vec<T> {
+//     a.sort_by(|x,y| { x.partial_cmp(y).unwrap() });
+//     a.dedup();
+//     a
+// }
+
+// // advanced 2: keep items in order
+// fn unique(a: Iterable<T>) -> Vec<T> {
+//     todo!();
+// }
+
+// // advanced 3: use iterators
+// fn unique(a: Iterable<T>) -> Vec<T> {
+//     todo!();
+// }
 
 fn main() {
-    let a = "?";
-    let b = "?".to_string();
-    info(&a);
-    info(&b);
-
-    // Advanced 1
-    // use std::ffi::CString;
-    
-    // let c = CString::new("?").unwrap();
-    // info(&input);
-
-    // Advanced 2
-    // use std::path::Path;
-    // let d = Path::new("/tmp/linkedin-learning");
-    // info(d);
+    let input = vec![2, 1, 1];
+    let answer = unique(input);
+    println!("unique items -> {:?}", answer)
 }
 
 
 #[test]
-fn str() {
-    let input = "Rust";
-    info(&input);
+fn empty_list() {
+    let input: Vec<i32> = vec![];
+    let expected_output: Vec<i32> = vec![];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
 }
 
 #[test]
-fn string() {
-    let input = String::from("Rust");
-    info(&input);
+fn sorted_list() {
+    let input = vec![1, 4, 5];
+    let expected_output = vec![1, 4, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
 }
 
-// #[test]
-// fn chars() {
-//     let input = 'r';
-//     info(&input);
-// }
+#[test]
+fn unsorted_list() {
+    let input = vec![1, 5, 2];
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
 
-// #[test]
-// fn cstring() {
-//     use std::ffi::{CString};
-//     let input = CString::new("Rust").unwrap();
-//     info(&input);
-// }
 
-// #[test]
-// fn path() {
-//     use std::path::Path;
-//     let input = Path::new("/tmp/rust");
-//     info(input);
-// }
+#[test]
+fn unsorted_list_with_duplicates() {
+    let input = vec![1, 5, 2, 2, 1];
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list_with_duplicates() {
+    let mut input = vec![1, 5, 2, 2, 1];
+    input.sort_by(|x,y| x.partial_cmp(y).unwrap());
+    let expected_output = vec![1, 2, 5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
---
+++ value-of-a-hand-of-cards/src/main.rs
@@ -0,0 +1,104 @@
+#[derive(Debug, PartialEq)]
+enum Card {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Jack,
+    Queen,
+    King,
+}
+
+struct Hand {
+    cards: Vec<Card>,
+}
+
+impl Hand {
+    fn new() -> Self {
+        Hand { cards: vec![] }
+    }
+
+    fn add(&mut self, card: Card) {
+        self.cards.push(card);
+    }
+
+    fn value(&self) -> usize {
+        // TODO: implement this method
+        use Card::*;
+        let mut value = 0;
+
+        self.cards.iter().for_each(|card| {
+            value += match card {
+                Ace => {
+                    if value + 11 <= 21 {
+                        11
+                    } else {
+                        1
+                    }
+                }
+                Two => 2,
+                Three => 3,
+                Four => 4,
+                Five => 5,
+                Six => 6,
+                Seven => 7,
+                Eight => 8,
+                Nine => 9,
+                Jack | Queen | King => 10,
+            }
+        });
+        value
+    }
+
+    fn is_loosing_hand(&self) -> bool {
+        self.value() > 21
+    }
+}
+
+fn main() {
+    let mut hand = Hand::new();
+    hand.add(Card::King);
+    hand.add(Card::Ace);
+}
+
+#[test]
+fn empty_hand() {
+    let hand = Hand::new();
+
+    assert_eq!(hand.value(), 0);
+}
+
+#[test]
+fn strong_hand() {
+    let mut hand = Hand::new();
+    hand.add(Card::Queen);
+    hand.add(Card::Ace);
+
+    assert_eq!(hand.value(), 21);
+}
+
+#[test]
+fn risky_hand() {
+    let mut hand = Hand::new();
+    hand.add(Card::King);
+    hand.add(Card::Queen);
+    hand.add(Card::Ace);
+
+    assert_eq!(hand.value(), 21);
+}
+
+#[test]
+fn oops() {
+    let mut hand = Hand::new();
+    hand.add(Card::King);
+    hand.add(Card::Seven);
+    hand.add(Card::Five);
+
+    assert!(hand.is_loosing_hand());
+    assert_eq!(hand.value(), 22);
+}
